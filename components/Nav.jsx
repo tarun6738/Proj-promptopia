a5bb6c36ab2959334a02da0bd710082b3f0b2455
tarun6738 <tarunnemali2004@gmail.com>
2023-09-29 19:18:07 +0530

    new changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -45,7 +45,7 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src={session?.user.image}
+                src="/assets/images/profile.svg"
                 width={37}
                 height={37}
                 className='rounded-full'
